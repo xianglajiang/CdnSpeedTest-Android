138d0c197ffaf6b934c59e1f0d62aba3ca729a75
Build <build@github.com>
2026-03-14 19:24:36 +0800

    CDN Speed Test Android App

---
+++ android/app/src/main/java/com/cdnspeedtest/app/MainActivity.java
@@ -0,0 +1,26 @@
+package com.cdnspeedtest.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        WebView webView = new WebView(this);
+        setContentView(webView);
+        
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        
+        webView.setWebViewClient(new WebViewClient());
+        
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
